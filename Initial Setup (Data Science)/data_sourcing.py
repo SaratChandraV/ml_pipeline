cc669f45f7464591fb81c817662b8a1b4f7e1517
Sarat Chandra <mail2saratchandrav@gmail.com>
2024-02-25 17:32:36 -0800

    init

--- Initial Setup (Data Science)/data_sourcing.py
+++ Initial Setup (Data Science)/data_sourcing.py
@@ -17,12 +17,18 @@ for ticker in tickers:
 
 db_params = keys.db_params.db_params
 
-def create_tables(stock_data):
-    conn = psycopg2.connect(**db_params)
-    cursor = conn.cursor()
-    
-    for ticker in stock_data:
-        table_name = ticker + "_daily_stock_data"
+def get_source_table_name(ticker):
+    return ticker + "_daily_stock_data"
+
+def get_source_table_trigger_name(ticker):
+    return get_source_table_name(ticker=ticker) + "_last_modified_trigger"
+
+def drop_source_table(ticker,cursor):
+    table_name = get_source_table_name(ticker=ticker)
+    cursor.execute(f"""DROP TABLE IF EXISTS {table_name}""")
+
+def create_source_table(ticker,cursor):
+        table_name = get_source_table_name(ticker=ticker)
         cursor.execute(f"""
             CREATE TABLE IF NOT EXISTS {table_name} (
                 date DATE PRIMARY KEY,
@@ -30,12 +36,78 @@ def create_tables(stock_data):
                 high NUMERIC,
                 low NUMERIC,
                 close NUMERIC,
-                volume BIGINT
+                volume BIGINT,
+                created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                last_modified_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
             );
         """)
+
+#create update trigger for last modification
+def create_update_trigger(ticker,cursor):
+    trigger_function_name = get_source_table_trigger_name(ticker=ticker)
+    trigger_name = get_source_table_trigger_name(ticker=ticker)
+    table_name = get_source_table_name(ticker=ticker)
+
+    cursor.execute(f"""
+        CREATE OR REPLACE FUNCTION {trigger_function_name}() 
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.last_modified_at = CURRENT_TIMESTAMP;
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+        
+        DROP TRIGGER IF EXISTS {trigger_name} ON {table_name};
+        
+        CREATE TRIGGER {trigger_name}
+        BEFORE UPDATE ON {table_name}
+        FOR EACH ROW EXECUTE FUNCTION {trigger_function_name}();
+    """)
+
+def setup_database(tickers):
+    conn = psycopg2.connect(**db_params)
+
+    #Drop tables if exists
+    cursor = conn.cursor()
+    for ticker in tickers:
+        drop_source_table(ticker=tickers,cursor=cursor)
     conn.commit()
     cursor.close()
+
+    #Create Tables
+    cursor = conn.cursor()
+    for ticker in tickers:
+        create_source_table(ticker=tickers,cursor=cursor)
+    conn.commit()
+    cursor.close()
+
+    #Create Triggers
+    cursor = conn.cursor()
+    for ticker in tickers:
+        create_update_trigger(ticker=tickers,cursor=cursor)
+    conn.commit()
+    cursor.close()
+
     conn.close()
 
-create_tables(stock_data)
+setup_database(tickers)
 
+def insert_stock_data(ticker,cursor,data):
+    table_name = get_source_table_name(ticker=ticker)
+    for index, row in data.iterrows():
+        cursor.execute(f"""
+            INSERT INTO {table_name} (date, open, high, low, close, volume)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON CONFLICT (date) DO NOTHING;
+        """, (index.date(), row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
+
+def insert_source_data(tickers,data):
+    conn = psycopg2.connect(**db_params)
+    for ticker in tickers:
+        cursor = conn.cursor()
+        insert_stock_data(ticker=ticker,cursor=cursor,data=data[ticker])
+        conn.commit()
+        cursor.close()
+    conn.close()
+
+insert_source_data(tickers=tickers,data=stock_data)
